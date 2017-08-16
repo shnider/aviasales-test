b4aabb493efdcfaa86eb710818bd5de4dad9ac54
shnider <shnider7860@gmail.com>
2017-08-16 09:45:22 +0300

    chore: sort ticket by price

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// ticket data from json
+import data from './ticket.json'
+
+const { tickets = [] } = data;
+const sortedTickets = tickets.slice()
+  .sort((obj1, obj2) => (obj1.price - obj2.price))
 
 class App extends Component {
+
+  
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       
       </div>
     );
   }
