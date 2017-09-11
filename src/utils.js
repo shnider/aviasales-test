7a5cfe428c47dd126a20225a50423ed94872682e
shnider <shnider7860@gmail.com>
2017-09-11 23:21:32 +0300

    chore: utils for get maximum stops

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,13 @@
-const sort = (array, value = 'price') =>
+// sort array of objects by one value
+export const sortByValue = (array, value = 'price') =>
   array.slice().sort((obj1, obj2) => obj1[value] - obj2[value]);
 
-export default sort;
+export const getMaximumStops = (array) => {
+  const ticketMaxStops = array.reduce((current, ticket) => {
+    if (ticket.stops > current.stops) {
+      return ticket;
+    }
+    return current;
+  });
+  return ticketMaxStops.stops;
+};
